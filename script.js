635edd58443bb20eae37d20decba50e9a71160ad
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-21 22:30:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
+let bandNames = ['The Rolling Stones', 'Aerosmith', 'The Beatles', 'Nirvana'];
+
+// Function to remove articles from band names
+function removeArticles(name) {
+  return name.replace(/^(a|an|the)\s+/i, '');
+}
+
+// Sort the band names in lexicographic order excluding articles
+bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Create the list of band names using <ul> and <li> tags
+let ul = document.createElement('ul');
+ul.id = 'band';
+
+for (let i = 0; i < bandNames.length; i++) {
+  let li = document.createElement('li');
+  li.textContent = bandNames[i];
+  ul.appendChild(li);
+}
+
+// Append the list to the document body or any other element
+document.body.appendChild(ul);
 
